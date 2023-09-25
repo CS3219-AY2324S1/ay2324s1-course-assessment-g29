4e1e3d0efe29aee22d25e3a35311e388e1bc8997
KT27Learn <66994197+KT27Learn@users.noreply.github.com>
2023-09-26 03:32:17 +0800

    Implement simple create room feature in collab service

---
+++ microservices/collabService/models/room-model.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const roomSchema = new Schema({
+    user1id: {type: String, required: true},
+    user2id: {type: String, required: true},
+}, {
+    timestamps: true,
+});
+
+const Room = mongoose.model('Room', roomSchema);
+
+module.exports = Room;
---
+++ microservices/collabService/routes/createroom.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const router = require('express').Router();
+const axios = require('axios');
+
+const Room = require('../models/room-model');
+
+router.route('/').post(async (req, res) => {
+    try {
+        const {user1id, user2id} = req.body;
+        const existing1 = await Room.findOne({ user1id});
+        if (existing1) {
+            return res.status(400).json({ message: "Please end previous collab session" });
+        }
+
+        const existing2 = await Room.findOne({ user2id });
+        if (existing2) {
+            return res.status(400).json({ message: "Unexpected error please rejoin the collab session" });
+        }
+        const matchResult = await Room.create({ user1id, user2id });
+        res.status(200).json({roomId: matchResult._id, message:"Room succesfully created!"});
+    } catch (error) {
+        res.status(200).json({roomId: matchResult._id, message:"Unexpected Error"});
+
+    }
+    
+});
+
+module.exports = router;
