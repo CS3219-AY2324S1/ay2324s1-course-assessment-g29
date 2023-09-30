1ec13c88f4f16aff46f71a09810501d8d6a8b7c9
xzzz3 <xingzheng@u.nus.edu>
2023-09-30 14:19:57 +0800

    Create REST API

---
+++ microservice/questionService/index.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+
+// Set up
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(express.json());
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server Started at ${port}`);
+});
+
+// Connecting to MongoDB
+const mongoString = process.env.DATABASE_URL;
+mongoose.connect(mongoString);
+const database = mongoose.connection;
+
+database.on("error", (error) => {
+  console.log(error); // log if connection error
+});
+
+database.once("connected", () => {
+  console.log("Database Connected"); // log if connection successful
+});
+
+// Import route file
+const routes = require("./routes/routes");
+
+app.use("/api", routes);
