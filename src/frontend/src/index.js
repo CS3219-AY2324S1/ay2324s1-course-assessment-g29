b99d4d1cfcc363ad79c364595b732f564c156e91
KT27Learn <66994197+KT27Learn@users.noreply.github.com>
2023-09-30 21:47:12 +0800

    Fix style errors for frontend service

--- src/frontend/src/index.js
+++ src/frontend/src/index.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import { PersistGate } from 'redux-persist/integration/react'
-import './index.css';
-import App from './App';
+import './index.css'
+import App from './App'
 import { Provider } from 'react-redux'
-import { persistor, store } from './redux/store';
+import { persistor, store } from './redux/store'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <App/>
+        <App />
       </PersistGate>
     </Provider>
   </React.StrictMode>
-);
+)
--- src/frontend/src/pages/HomePage.js
+++ src/frontend/src/pages/HomePage.js
@@ -1,67 +1,62 @@
-import { useState } from 'react';
+import { useState } from 'react'
 import { useDispatch } from 'react-redux'
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom'
 import { setRoomId, setUserId } from '../redux/MatchingSlice'
-import axios from 'axios';
-import Alert from '@mui/material/Alert';
-import Box from '@mui/material/Container';
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
-import Container from '@mui/material/Container';
+import axios from 'axios'
+import Alert from '@mui/material/Alert'
+import Box from '@mui/material/Container'
+import Button from '@mui/material/Button'
+import TextField from '@mui/material/TextField'
+import Container from '@mui/material/Container'
 
-function HomePage(props) {
-    const [showErrorAlert, setShowErrorAlert] = useState(false);
-    const [showSuccessAlert, setShowSuccessAlert] = useState(false);
-    const [successMessage, setSuccessMessage] = useState('');
-    const [errorMessage, setErrorMessage] = useState('');
-    const [currentUserId, setCurrentUserId] = useState('');
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
+function HomePage () {
+  const [showErrorAlert, setShowErrorAlert] = useState(false)
+  const [showSuccessAlert, setShowSuccessAlert] = useState(false)
+  const [successMessage, setSuccessMessage] = useState('')
+  const [errorMessage, setErrorMessage] = useState('')
+  const [currentUserId, setCurrentUserId] = useState('')
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
 
-    const joinQueue = async () => {
-        if (!currentUserId) {
-            setErrorMessage('Enter a user id first')
-            setShowErrorAlert(true);
-            return;
-        }
-        try {
-            console.log("Try Matching")
-            const response = await axios.post('http://localhost:4000/match/', {
-                user1id: currentUserId,
-            });
-
-            dispatch(setRoomId(response.data.roomId));
-            
-            dispatch(setUserId(currentUserId));
-            setSuccessMessage(response.data.message);
-            setShowSuccessAlert(true);
-            navigate('/collab')
-            
-        } catch (error) {
-            setErrorMessage(error.message)
-            setShowErrorAlert(true);
-            return;
-        }
+  const joinQueue = async () => {
+    if (!currentUserId) {
+      setErrorMessage('Enter a user id first')
+      setShowErrorAlert(true)
+      return
     }
-
-    return (
-        <>
-            <Container>
-                { showSuccessAlert ? (
-                    <Alert onClose={() => setShowSuccessAlert(false)}>{successMessage}</Alert>
-                ) : (<></>)}
-                { showErrorAlert ? (
-                    <Alert severity="error" onClose={() => setShowErrorAlert(false)}>Error: {errorMessage}</Alert>
-                ) : (<></>)}
-                
-                <Box mt={2}>
-                <TextField value={currentUserId} onChange={(e) => setCurrentUserId(e.target.value)} id="outlined-basic" label="UserId" variant="outlined" />
-                <Button variant="contained" onClick={joinQueue}>Start Match</Button>
-                </Box>
-            </Container>
-        </>
-    );
-
+    try {
+      console.log('Try Matching')
+      const response = await axios.post('http://localhost:4000/match/', {
+        user1id: currentUserId
+      })
+      dispatch(setRoomId(response.data.roomId))
+      dispatch(setUserId(currentUserId))
+      setSuccessMessage(response.data.message)
+      setShowSuccessAlert(true)
+      navigate('/collab')  
+    } catch (error) {
+      setErrorMessage(error.message)
+      setShowErrorAlert(true)
+      return
+    }
+  }
+  
+  return (
+    <>
+      <Container>
+        { showSuccessAlert ? (
+          <Alert onClose={() => setShowSuccessAlert(false)}>{successMessage}</Alert>
+        ) : (<></>)}
+        { showErrorAlert ? (
+          <Alert severity='error' onClose={() => setShowErrorAlert(false)}>Error: {errorMessage}</Alert>
+        ) : (<></>)}        
+        <Box mt={2}>
+          <TextField value={currentUserId} onChange={(e) => setCurrentUserId(e.target.value)} id='outlined-basic' label='UserId' variant='outlined' />
+          <Button variant='contained' onClick={joinQueue}>Start Match</Button>
+        </Box>
+      </Container>
+    </>
+  )
 }
 
-export default HomePage;
+export default HomePage
