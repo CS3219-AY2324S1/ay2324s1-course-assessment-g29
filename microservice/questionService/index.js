862204073b370c616f3a3838265358058b9b04f8
xzzz3 <xingzheng@u.nus.edu>
2023-10-02 21:33:36 +0800

    Port fix

--- microservice/questionService/index.js
+++ microservice/questionService/index.js
@@ -8,7 +8,7 @@ const app = express()
 
 app.use(express.json())
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3002
 
 app.listen(port, () => {
   console.log(`Server Started at ${port}`)
