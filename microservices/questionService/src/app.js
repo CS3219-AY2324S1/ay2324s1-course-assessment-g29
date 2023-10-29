736ab30e383c2429589995bd4ff572a24b073a66
benedictchuajj <benedictchuajj@gmail.com>
2023-10-29 14:14:48 +0800

    Fix missing mongoose connection

--- microservices/questionService/src/app.js
+++ microservices/questionService/src/app.js
@@ -15,4 +15,22 @@ app.get('/', (req, res) => {
 
 app.use('/question', routes)
 
+// Connecting to MongoDB
+const mongoose = require('mongoose')
+const path = require('path')
+const envPath = path.join(__dirname, '../.env')
+require('dotenv').config({ path: envPath })
+
+const mongoString = process.env.DATABASE_URL
+mongoose.connect(mongoString)
+const database = mongoose.connection
+
+database.on('error', (error) => {
+  console.log(error) // log if connection error
+})
+
+database.once('connected', () => {
+  console.log('Database Connected') // log if connection successful
+})
+
 module.exports = app
