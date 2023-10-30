6650958685781865cf7b7a6bfe32c968ce6bc291
benedictchuajj <benedictchuajj@gmail.com>
2023-10-30 16:05:21 +0800

    Update collab unit tests

--- microservices/collabService/src/tests/unit.spec.js
+++ microservices/collabService/src/tests/unit.spec.js
@@ -2,47 +2,125 @@
 
 const chai = require('chai')
 const chaiHttp = require('chai-http')
-const app = require('../../app')
-const expect = chai.expect
+const sinon = require('sinon')
+const axios = require('axios')
+const admin = require('firebase-admin')
 
 chai.use(chaiHttp)
+const expect = chai.expect
 
-describe('Express App Unit Tests', () => {
-  it('should create a new room', (done) => {
-    chai
-      .request(app)
-      .post('/room/createroom')
-      .send({
-        user1id: 'user123',
-        user2id: 'user456',
-        matchingLanguages: ['JavaScript', 'Python']
+// Stubs
+const fakeQuestionData = {
+  _id: '653408cca7a26f448a0fc147',
+  name: 'roman-to-integer',
+  displayName: 'Roman To Integer',
+  description: 'Beep Boop',
+  topic: [
+    'algorithms'
+  ],
+  imagesArray: [],
+  difficulty: 'Easy',
+  __v: 0
+}
+const matchingLanguages = ['Python']
+
+const axiosStub = sinon.stub(axios, 'get')
+axiosStub.withArgs('http://questionservice:3002/question/getRandom').resolves({
+  data: fakeQuestionData
+})
+
+const firestoreStub = sinon.stub(admin, 'firestore')
+const collectionStub = sinon.stub()
+
+firestoreStub.get(function () {
+  return function () {
+    return { collection: collectionStub }
+  }
+})
+
+collectionStub.withArgs('rooms').returns({
+  get: sinon.stub().resolves(
+    [{
+      data: () => ({
+        user1id: 'user1',
+        user2id: 'user2',
+        questionData: fakeQuestionData,
+        matchingLanguages
       })
-      .end((res) => {
-        expect(res).to.have.status(200)
-        expect(res.body).to.have.property('roomId')
-        expect(res.body).to.have.property('questionData')
-        done()
-      })
+    }]
+  ),
+  doc: sinon.stub().callsFake((rid) => {
+    return {
+      set: sinon.stub().resolves({ message: 'Done!' }),
+      update: sinon.stub().resolves({ message: 'Done!' }),
+      delete: sinon.stub().resolves({ message: 'Done!' })
+    }
+  })
+})
+
+collectionStub.withArgs('history').returns({
+  doc: sinon.stub().callsFake((rid) => {
+    return {
+      set: sinon.stub().resolves({ message: 'Done!' }),
+      update: sinon.stub().resolves({ message: 'Done!' })
+    }
+  })
+})
+
+collectionStub.withArgs('useridToRoom').returns({
+  doc: sinon.stub().callsFake((uid) => {
+    return {
+      set: sinon.stub().resolves({ message: 'Done!' }),
+      get: sinon.stub().resolves({ data: () => ({ roomId: [] }) })
+    }
+  })
+})
+
+// Import functions to test
+const { createRoom, checkRoom, saveHistory, updateHistory, changeQuestion, leaveRoom } = require('../controller/roomController')
+
+describe('Unit Test for /collab', () => {
+  it('should create a new room', async () => {
+    const req = { body: { user1id: 'user1', user2id: 'user2', matchingLanguages } }
+    const res = {
+      status: (code) => {
+        expect(code).to.equal(200)
+        return {
+          json: (data) => {
+            expect(data).to.have.property('roomId')
+            expect(data).to.have.property('message')
+          }
+        }
+      }
+    }
+
+    // Test the createRoom function
+    await createRoom(req, res)
+
+    // Restore the stub after the test
+    axiosStub.restore()
   })
 
-  it('should check for an existing room', (done) => {
-    chai
-      .request(app)
-      .post('/room/checkroom')
-      .send({ userid: 'user123' })
-      .end((res) => {
-        expect(res).to.have.status(200)
-        expect(res.body).to.have.property('room')
-        expect(res.body).to.have.property('roomdata')
-        done()
-      })
+  it('should check for an existing room', async () => {
+    const req = { body: { userid: 'user1' } }
+    const res = {
+      status: (code) => {
+        expect(code).to.equal(200)
+        return {
+          json: (data) => {
+            expect(data).to.have.property('room')
+            expect(data).to.have.property('roomdata')
+          }
+        }
+      }
+    }
+
+    await checkRoom(req, res)
   })
 
-  it('should save room history', (done) => {
-    chai
-      .request(app)
-      .post('/room/savehistory')
-      .send({
+  it('should save room history', async () => {
+    const req = {
+      body: {
         rid: 'room_id',
         user1id: 'user123',
         user2id: 'user456',
@@ -50,54 +128,77 @@ describe('Express App Unit Tests', () => {
         code: 'your_code',
         language: 'your_language',
         messages: ['message1', 'message2']
-      })
-      .end((res) => {
-        expect(res).to.have.status(200)
-        done()
-      })
+      }
+    }
+    const res = {
+      status: (code) => {
+        expect(code).to.equal(200)
+        return {
+          json: (data) => {
+            expect(data).to.have.property('message')
+          }
+        }
+      }
+    }
+    await saveHistory(req, res)
   })
 
-  it('should update room history', (done) => {
-    chai
-      .request(app)
-      .post('/room/updatehistory')
-      .send({
+  it('should update room history', async () => {
+    const req = {
+      body: {
         rid: 'room_id', // Provide a valid room ID
         questionData: 'updated_question_data',
         code: 'updated_code',
         language: 'updated_language',
         messages: ['updated_message1', 'updated_message2']
-      })
-      .end((res) => {
-        expect(res).to.have.status(200)
-        done()
-      })
+      }
+    }
+    const res = {
+      status: (code) => {
+        expect(code).to.equal(200)
+        return {
+          json: (data) => {
+            expect(data).to.have.property('message')
+          }
+        }
+      }
+    }
+    await updateHistory(req, res)
   })
 
-  it('should change question in a room', (done) => {
-    chai
-      .request(app)
-      .post('/room/changequestion')
-      .send({
+  it('should change question in a room', async () => {
+    const req = {
+      body: {
         rid: 'room_id',
         questionData: 'new_question_data'
-      })
-      .end((res) => {
-        expect(res).to.have.status(200)
-        done()
-      })
+      }
+    }
+    const res = {
+      status: (code) => {
+        expect(code).to.equal(200)
+        return {
+          json: (data) => {
+            expect(data).to.have.property('message')
+          }
+        }
+      }
+    }
+
+    await changeQuestion(req, res)
   })
 
-  it('should delete a room', (done) => {
-    chai
-      .request(app)
-      .post('/room/leaveroom')
-      .send({
-        rid: 'room_id'
-      })
-      .end((res) => {
-        expect(res).to.have.status(200)
-        done()
-      })
+  it('should delete a room', async () => {
+    const req = { body: { rid: 'room_id' } }
+    const res = {
+      status: (code) => {
+        expect(code).to.equal(200)
+        return {
+          json: (data) => {
+            expect(data).to.have.property('message')
+          }
+        }
+      }
+    }
+    await leaveRoom(req, res)
   })
 })
